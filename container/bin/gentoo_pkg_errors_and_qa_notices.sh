e484ea9dbbf597aaaa6dcb1422a9a985ca0e6a42
Joonas Niilola <juippis@gentoo.org>
2024-02-03 16:35:59 +0200

    container/bin/gentoo_pkg_errors_and_qa_notices.sh: add file

---
+++ container/bin/gentoo_pkg_errors_and_qa_notices.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# gentoo-gh-prtester.sh: Find all build errors fast, find all QA notices after emerge for the 
+# atom we just tested.
+#
+# gentoo-pkg-rdeptester.sh: With pkg-testing-tool --report, and with a custom "test.conf", 
+# find out exact errors easier.
+#
+
+if [[ -d "/var/tmp/portage/vbslogs/build/" ]]; then
+	echo "Grepping through the logs for any errors or QA notices..."
+	grep -r "QA Notice:" /var/tmp/portage/vbslogs/build/
+	grep -r "ERROR:" /var/tmp/portage/vbslogs/build/ | grep "failed"
+	grep -r "* Failed to" /var/tmp/portage/vbslogs/build/
+	grep -r "installing to one or more unexpected paths" /var/tmp/portage/vbslogs/build/
+	grep -r "Permission denied" /var/tmp/portage/vbslogs/build/
+	grep -r "command not found" /var/tmp/portage/vbslogs/build/
+	grep -r "WARNING: Unknown options:" /var/tmp/portage/vbslogs/build/
+	grep -r "One or more CMake variables were not used by the project" /var/tmp/portage/vbslogs/build/
+	grep -r "QA:" /var/tmp/portage/vbslogs/build/
+	grep -r "fatal:" /var/tmp/portage/vbslogs/build/
+	echo "Finished grepping."
+
+else
+	echo "No desired atoms were tested apparently? Exiting..."
+
+fi
