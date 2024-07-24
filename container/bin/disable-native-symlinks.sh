75fb66caa5f8dba9a38359c76795314a1c7d7e5d
Joonas Niilola <juippis@gentoo.org>
2024-07-24 09:26:43 +0300

    container/bin/disable-native-symlinks.sh: update package.use.force logic
     - it can be a directory, or a single file.

--- container/bin/disable-native-symlinks.sh
+++ container/bin/disable-native-symlinks.sh
@@ -4,25 +4,29 @@
 # discardable containers, ie, does not "toggle" the state of native-symlinks.
 
 mkdir -p /etc/portage/profile/
+
+if [[ -d /etc/portage/profile/package.use.force ]] ; then
+	puftarget="/etc/portage/profile/package.use.force/native-symlinks"
+else
+	puftarget="/etc/portage/profile/package.use.force"
+fi
+
 {
 	echo "dev-lang/python-exec -native-symlinks"
 	echo "sys-devel/binutils-config -native-symlinks"
 	echo "sys-devel/gcc-config -cc-wrappers -native-symlinks"
-} > /etc/portage/profile/package.use.force
+} >> "${puftarget}"
 
-if [[ -d /etc/portage/package.use ]]; then
-	{
-		echo "dev-lang/python-exec -native-symlinks"
-		echo "sys-devel/binutils-config -native-symlinks"
-		echo "sys-devel/gcc-config -cc-wrappers -native-symlinks"
-	} > /etc/portage/package.use/native-symlinks
-
+if [[ -d /etc/portage/package.use ]] ; then
+	putarget="/etc/portage/package.use/native-symlinks"
 else
-	{
+	putarget="/etc/portage/package.use"
+fi
+
+{
 	echo "dev-lang/python-exec -native-symlinks"
 	echo "sys-devel/binutils-config -native-symlinks"
-	echo "sys-devel/gcc-config -native-symlinks"
-	} >> /etc/portage/package.use
-fi
+	echo "sys-devel/gcc-config -cc-wrappers -native-symlinks"
+} >> "${putarget}"
 
 emerge -1av --usepkg-exclude dev-lang/python-exec --usepkg-exclude sys-devel/binutils-config --usepkg-exclude sys-devel/gcc-config dev-lang/python-exec sys-devel/binutils-config sys-devel/gcc-config
