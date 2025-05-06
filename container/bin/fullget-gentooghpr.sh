49cdf4caa68b2903eb83916275dcb4045dd2c6d7
Joonas Niilola <juippis@gentoo.org>
2025-05-06 15:03:06 +0300

    container/bin/fullget-gentooghpr.sh: rewrite
    Github has started aggressively limiting frequent requests when you're not
    logged in. This is an attempt to slow down fetching, while still keeping the
    whole process as much automated as possible. This is only an issue if you have
    a habit of launching multiple PR testers at once, or within short intervals.

--- container/bin/fullget-gentooghpr.sh
+++ container/bin/fullget-gentooghpr.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-# Just a simple PR patch merger. It's much more wise to set up your git config with (any) 
+# Just a simple PR patch merger. It's much more wise to set up your git config with (any)
 # user.email and user.name, but this in general should work without setup everywhere.
+#
+# Github is limiting frequent requests when you're not logged in. Add a loop and delay to
+# keep using this without API keys, for now.
 
 cd /var/db/repos/gentoo || exit
 
-curl -s -L https://patch-diff.githubusercontent.com/raw/gentoo/gentoo/pull/${1}.patch | git -c user.email=my@email -c user.name=MyName am --keep-cr -3
+# Wait for a bit over 2 minutes - this seems to work.
+# 1 minute is too short.
+sleepdelay=125
+ghpr="https://github.com/gentoo/gentoo/pull/${1}.patch"
+outputpatchfilename="gentoo-github-pr-${1}"
+
+while true; do
+	wget -O /tmp/"${outputpatchfilename}".patch "${ghpr}"
+
+	if [ $? -eq 8 ]; then
+		echo "Received HTTP 429: Too Many Requests. Retrying in ${sleepdelay} seconds."
+		sleep ${sleepdelay}
+	else
+		git -c user.email=my@email -c user.name=MyName am --keep-cr -3 /tmp/"${outputpatchfilename}".patch
+		break
+	fi
+done
